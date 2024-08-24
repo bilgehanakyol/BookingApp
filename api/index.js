8fabb638e9528b2c740a219e8e80592880acd474
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-08-24 20:58:54 +0300

    upload by link

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { default: mongoose } = require('mongoose');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 require('dotenv').config();
+const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'domystringasd';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors(
     {
         credentials: true,
@@ -79,6 +81,16 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => { 
     res.cookie('token', '').json(true); 
+});
+
+app.post('/upload-by-link', async (req, res) => {
+    const {link} = req.body;
+    const newName = 'photo' + Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/uploads/' + newName,
+    });
+    res.json(newName);
 });
 
 app.listen(4000, () => {
