b5446d8a8c3d38224774bdbc918162672bd24cac
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-08-27 14:40:39 +0300

    delete and select photo

--- api/index.js
+++ api/index.js
@@ -122,7 +122,7 @@ app.post('/places', (req, res) => {
         res.json(placeDoc);
     });
 });
-app.get('/places', (req,res) => {
+app.get('/user-places', (req,res) => {
     const {token} = req.cookies;
     jwt.verify(token, jwtSecret, {}, async (err, userData) => {
         const {id} = userData;
